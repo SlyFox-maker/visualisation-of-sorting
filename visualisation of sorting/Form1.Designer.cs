7b69944f9eb0faa4fd9d2a376a0a34474590a208
SlyFox-maker <skyfox4995@gmail.com>
2022-10-24 10:16:13 -0500

    Добавьте файлы проекта.

---
+++ visualisation of sorting/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace visualisation_of_sorting
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Image = global::visualisation_of_sorting.Properties.Resources.preview;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 40);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(546, 266);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(481, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(77, 24);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Draw Array";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.SystemColors.MenuText;
+            this.comboBox1.ForeColor = System.Drawing.SystemColors.Info;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Update..."});
+            this.comboBox1.Location = new System.Drawing.Point(12, 10);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 23);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.TextChanged += new System.EventHandler(this.comboBox1_TextChanged);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.numericUpDown1.ForeColor = System.Drawing.SystemColors.Info;
+            this.numericUpDown1.Increment = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Location = new System.Drawing.Point(245, 10);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            500,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(57, 23);
+            this.numericUpDown1.TabIndex = 4;
+            this.numericUpDown1.ThousandsSeparator = true;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.numericUpDown2.ForeColor = System.Drawing.SystemColors.Info;
+            this.numericUpDown2.Increment = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Location = new System.Drawing.Point(139, 10);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(100, 23);
+            this.numericUpDown2.TabIndex = 5;
+            this.numericUpDown2.ThousandsSeparator = true;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.ToolTipTitle = "Algorithm";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button2.Enabled = false;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.Location = new System.Drawing.Point(398, 10);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(77, 24);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Start";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(570, 318);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Button button1;
+        private ComboBox comboBox1;
+        private NumericUpDown numericUpDown1;
+        private NumericUpDown numericUpDown2;
+        private ToolTip toolTip1;
+        private Button button2;
+    }
+}
---
+++ visualisation of sorting/Form1.cs
@@ -0,0 +1,228 @@
+using Medallion;
+using System.Diagnostics;
+using IronPython.Hosting;
+using Microsoft.Scripting.Hosting;
+using IronPython.Runtime.Operations;
+using static IronPython.Modules._ast;
+using SharpDX.DXGI;
+
+namespace visualisation_of_sorting
+{
+    public partial class Form1 : Form
+    {
+
+        private int sizeOfArray = 0;
+        private int delay = 0;
+        private String algorithm = "";
+
+        private double[] arraySorting;
+        private bool sorting = false;
+
+        private String path = "./algorithms/";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //description for elements
+            ToolTip algTP = new ToolTip();
+
+            algTP.SetToolTip(comboBox1, "Choise algorithm");
+            algTP.SetToolTip(numericUpDown2, "Size of array");
+            algTP.SetToolTip(numericUpDown1, "Delay");
+
+            pictureBox1.Image = Properties.Resources.preview as Bitmap;
+
+            readFiles();
+        }
+        private void readFiles()
+        {
+            comboBox1.Items.Clear();
+            comboBox1.Items.Add("Update...");
+
+            string[] files = Directory.GetFiles(path, "*", SearchOption.AllDirectories);
+            foreach (string filename in files)
+            {
+                String name = Path.GetFileNameWithoutExtension(filename);
+                if (Path.GetExtension(filename) == ".py")
+                {
+                    comboBox1.Items.Add(name);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            sizeOfArray = Convert.ToInt32(numericUpDown2.Value) + 1;
+
+            //Creating array
+            generationRandomNumbers();
+
+            //First painting
+            paintArrays(0,0);
+
+            button2.Enabled = true;
+        }
+        public void paintArrays(int firstElement, int secondElement)
+        {
+            Bitmap bit = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            Graphics g = Graphics.FromImage(bit);
+
+            //initialization sizes
+            double widthLines = Math.Floor((double)bit.Width / (double)sizeOfArray);
+            Pen p;
+            int startPointX = (int)widthLines;
+            int startPointY = bit.Height;
+            int unitHeight = bit.Height / sizeOfArray;
+
+            //painting
+            for (int i = 1; i < sizeOfArray; i++)
+            {
+                if (firstElement == i || secondElement==i)
+                {
+                    p = new Pen(Color.Red, (int)widthLines);
+                }
+                else
+                {
+                    p = new Pen(Color.White, (int)widthLines);
+                }
+                //new posision
+                g.DrawLine(p, startPointX, startPointY, startPointX, (int)(startPointY - (unitHeight * arraySorting[i])));
+                startPointX += (int)widthLines + 1;
+                Debug.WriteLine(arraySorting[i]);
+            }
+
+            //putting to picturebox
+            pictureBox1.Image = bit;
+            g.Flush();
+        }
+        private void generationRandomNumbers() {
+            Random r = new Random();
+            arraySorting = new double[sizeOfArray];
+            for (int i = 1; i < sizeOfArray; i++)
+            {
+                arraySorting[i] = i;
+            }
+
+            arraySorting.Shuffle();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            switchButtonStart();
+            algorithm = (String)comboBox1.Text;
+
+            //checking
+            if (algorithm == ""||algorithm=="Update...")
+            {
+                MessageBox.Show("Algorithm was not choiced");
+                return;
+            }
+
+            Thread workerThread = new Thread(new ThreadStart(execute));
+            // Start secondary thread  
+            workerThread.Start();
+        }
+        private void switchButtonStart()
+        {
+            if (!sorting)
+            {
+                button2.Text = "Stop";
+                sorting = true;
+            }
+            else if (sorting)
+            {
+                if (button1.InvokeRequired)
+                {
+                    button2.Invoke(new MethodInvoker(delegate
+                    {
+                        button2.Text = "Start";
+                    }));
+                }
+                else
+                {
+                    button2.Text = "Start";
+                }
+                sorting = false;
+                return;
+            }
+        }
+        private void execute()
+        {
+            //Execute py scripts
+            String path = this.path + algorithm + ".py";
+            ScriptEngine engine = Python.CreateEngine();
+            ScriptScope scope = engine.CreateScope();
+
+            double[] arrayFilter = (double[])arraySorting.Clone();
+            try
+            {
+                scope.SetVariable("arr", arrayFilter);
+                engine.ExecuteFile(path, scope);  //start script
+
+                var arrHistory = scope.GetVariable("arrHistory");
+                var arrSort = scope.GetVariable("arr");
+
+                for (int i = 0; i < arrHistory.Count; i++)
+                {
+                    if (!sorting)
+                        return;
+                    delay = (int)numericUpDown1.Value;
+                    var parametres = arrHistory[i];
+
+
+                    int firstElement = parametres[1];
+                    int secondElement = parametres[2];
+
+                    var arrVar = parametres[0];
+                    double[] arr = new double[arrVar.Count];
+                    for (int w = 0; w < arrVar.Count; w++)
+                    {
+                        double d = arrVar[w];
+                        arr[w] = d;
+                    }
+
+                    arraySorting = arr;
+
+
+                    paintArrays(firstElement, secondElement);
+
+
+                    SoundUtilities.PlaySound("./check.wav", 0.1f);
+                    Thread.Sleep(delay);
+                }
+
+                finalPaint();
+            }
+            catch (IOException e)
+            {
+                switchButtonStart();
+                MessageBox.Show(e.Message);
+            }
+            catch(System.MissingMemberException e)
+            {
+                switchButtonStart();
+                MessageBox.Show(e.Message);
+            }
+        }
+        private void finalPaint()
+        {
+            paintArrays(0, 0);
+        }
+
+
+        private void comboBox1_TextChanged(object sender, EventArgs e)
+        {
+            if((String)comboBox1.Text == "Update...")
+            {
+                readFiles();
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            sorting = false;
+        }
+    }
+}
